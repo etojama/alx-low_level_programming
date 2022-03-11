652a00d5419e99144af5ebbf68610e4795cdc16d
etojama <torohkiss@gmail.com>
2022-03-10 16:01:39 -0800

    added semicolon

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-*main - Entry point
+* main - Entry point
 *
-* Return: 0 at the end of the program
+* Return: Always 0 (Success)
 *
 *
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
