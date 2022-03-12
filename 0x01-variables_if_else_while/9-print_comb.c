a02f0ad478029df71264aced1d663e0a0666f3e3
etojama <torohkiss@gmail.com>
2022-03-12 00:31:38 -0800

    print numbers in ascending order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - print number in ascending order
+* and putchar can only be used like 4 times
+*
+* Return: returns 0 at the end
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; i++)
+	{
+		putchar(i + '0');
+	if (i < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n')
+	return (0);
+	}
+}
+
