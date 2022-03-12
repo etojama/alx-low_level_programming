2ef7fc95596c95cd5e949b0847cb9a9a2fc330f3
etojama <torohkiss@gmail.com>
2022-03-12 01:45:27 -0800

    printed combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - program that prints two numbers 
+* in combination of two digits
+*
+* Return: returns zero at the end
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
