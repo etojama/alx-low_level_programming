a05e19bdf9ce3fc4075fe5da98242891ad2f03d2
etojama <torohkiss@gmail.com>
2022-03-12 02:40:33 -0800

    changed add to modulus

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 			if (j > i)
 			{
 				putchar((i / 10) + '0');
-				putchar((i / 10) % '0');
+				putchar((i % 10) + '0');
 				putchar(' ');
 				putchar((j / 10) + '0');
 				putchar((j % 10) + '0');
