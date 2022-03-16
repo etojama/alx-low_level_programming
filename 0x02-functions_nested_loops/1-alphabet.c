57884857d542ce3ff0e47654be6c8d2e35e7a858
etojama <torohkiss@gmail.com>
2022-03-15 17:06:21 -0700

    reworked comments

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
-* print alphabet - a function that prints alphabets in lowercase
+* Print alphabet - a function that prints alphabets in lowercase
 *
 * Return: Always 0
 */
+
 void print_alphabet(void)
 {
 	char a = 97;
+
 	while (a < 123)
 	{	_putchar(a);
 		a++;
 	}
 	_putchar(10);
-	return;
+
 }
