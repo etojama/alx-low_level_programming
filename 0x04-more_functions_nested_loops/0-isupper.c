7e482d17f3b6bc319adb6ae256518646dcbed1a0
etojama <torohkiss@gmail.com>
2022-03-17 08:10:50 -0700

    added more comments

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
-* 0-isupper - checks if a number is upper
+* -isupper - checks if a number is upper
+* @c: The integer used when the function is called
 *
-* Return: returns 1 if c is upper
+* Description: A function that identifies upper
+* case character
+* Return: 0 - SUCCESS
 */
 
 int _isupper(int c)
