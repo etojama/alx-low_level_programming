7ac8ba9eb15715d5b9d8feb44725c95080d4d2bd
etojama <torohkiss@gmail.com>
2022-03-17 08:30:24 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_numbers - The function that's called
+*
+* Description: A function that prints the numbers
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int l = 48;
+
+	while (l < 58)
+	{
+		_putchar(l);
+		i++;
+	}
+
+	_putchar(10);
+}
