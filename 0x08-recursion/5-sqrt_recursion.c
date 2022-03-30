53822d9a2b0126203494d0bc7689cb7e90642601
etojama <torohkiss@gmail.com>
2022-03-30 08:20:59 -0700

    added main

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,3 +22,19 @@ int helperFunction(int num, int pSqrt)
 			return (helperFunction(num, pSqrt + 1));
 	}
 }
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number to find sqrt of.
+ *
+ * Return: squareroot of n.
+ * -1 if n does not have a natural sqrt.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (helperFunction(n, 0));
+}
