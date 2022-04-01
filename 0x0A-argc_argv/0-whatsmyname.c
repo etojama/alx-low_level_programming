e2e16521ee65f7054e350597cb8ad43f557fdd9f
etojama <torohkiss@gmail.com>
2022-04-01 08:00:00 -0700

    corrected s

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,9 @@
  * Return: 0 on success.
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%d", argv[0]);
+
+	printf("%s\n", argv[0]);
 	return (0);
