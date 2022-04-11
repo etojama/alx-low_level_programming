1c337f7bce27d9f38ca137026815d6ee78bac709
etojama <torohkiss@gmail.com>
2022-04-11 08:59:21 -0700

    added equality operator

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,8 +17,10 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		if (d->name = NULL)
-			printf("Name: (nil)\n", d->name);
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
 
 		printf("Age: %f\n", d->age);
 
